d19f5d2168841089d85d0f949fc9cf58cbc0cc25
Othoniel Virgen <ov7664@gmail.com>
2018-06-20 23:22:25 -0500

    game is fully functional.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,109 @@
 // Beginning of the game
 
-$("document").ready(function(){
 
-    // Creates a random number between 18-120 and displays this number to the player.
 
-    var ranNum = Math.floor(Math.random() * 102) + 18;
-    $("#ran-num").text(ranNum);
+// Creates a random number between 19-120 and displays this number to the player.
 
-    // The winning and losing varaibles and displaying these numbers
+var ranNum = Math.floor(Math.random() * 101) + 19;
+$("#ran-num").text(ranNum);
 
-    var wins = 0;
-    $("#wins").text(wins);
+// The winning and losing varaibles and displaying these initial numbers.
 
-    var losses = 0;
-    $("#losses").text(losses);
+var wins = 0;
+$("#wins").text(wins);
+
+var losses = 0;
+$("#losses").text(losses);
     
-    // The crystals' number value between 1-12
+// The crystals' number value between 1-12.
 
-    var crys1 = Math.floor(Math.random() * 11) + 1;
-    var crys2 = Math.floor(Math.random() * 11) + 1;
-    var crys3 = Math.floor(Math.random() * 11) + 1;
-    var crys4 = Math.floor(Math.random() * 11) + 1;
+var crys1 = Math.floor(Math.random() * 11) + 1;
+var crys2 = Math.floor(Math.random() * 11) + 1;
+var crys3 = Math.floor(Math.random() * 11) + 1;
+var crys4 = Math.floor(Math.random() * 11) + 1;
 
-    // The user's total points guessed is displayed
+// The user's total initial points guessed is displayed.
 
-    var points = 0;
-    $("#points").text(points);
+var points = 0;
+$("#points").text(points);
 
-    // 
+// This function resets the game.
 
+function reset() {
+     ranNum = Math.floor(Math.random() * 101) + 19;
+    $("#ran-num").text(ranNum);
+        
+     crys1 = Math.floor(Math.random() * 11) + 1;
+     crys2 = Math.floor(Math.random() * 11) + 1;
+     crys3 = Math.floor(Math.random() * 11) + 1;
+     crys4 = Math.floor(Math.random() * 11) + 1;
+
+     points = 0;
+     $("#points").text(points);
+}
+
+// This function runs when the user wins.
+
+function victory() {
+    alert("YOU WON")
+    wins++;
+    $("#wins").text(wins);
+    reset();
+}
+
+// This last one runs when the user losses.
+
+function defeat() {
+    alert("YOU LOST")
+    losses++;
+    $("#losses").text(losses);
+    reset();
+}
+
+//Click events for the jewels
+
+$('#emerald').on ('click', function(){
+    points = points + crys1 ;
+    $('#points').text(points); 
+        if (points == ranNum){
+          victory();
+        }
+        else if ( points > ranNum){
+          defeat();
+        }   
+})  
+
+$('#diamond').on ('click', function(){
+    points = points + crys2;
+    $('#points').text(points); 
+        if (points == ranNum){
+         victory();
+        }
+        else if ( points > ranNum){
+          defeat();
+        } 
+})  
+
+$('#ruby').on ('click', function(){
+    points = points + crys3;
+    $('#points').text(points);
+          if (points == ranNum){
+         victory();
+        }
+        else if ( points > ranNum){
+          defeat();
+        } 
 })
+
+$('#sapphire').on ('click', function(){
+    points = points + crys4;
+    $('#points').text(points); 
+      
+          if (points == ranNum){
+         victory();
+        }
+        else if ( points > ranNum){
+          defeat();
+        }
+})  
+
